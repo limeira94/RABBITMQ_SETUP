4cb1c248186df2a39f506e002501ef61bac9ecba
Felipe Limeira <limeira.felipe94@gmail.com>
2023-06-26 17:44:08 -0300

    Aprendendo sobre RabbitMQ

---
+++ publisher_raw.py
@@ -0,0 +1,23 @@
+import pika
+
+
+connection_parameters = pika.ConnectionParameters(
+        host="localhost",
+        port=5672,
+        credentials=pika.PlainCredentials(
+            username="guest",
+            password="guest"
+        )
+    )
+
+channel = pika.BlockingConnection(connection_parameters).channel()
+
+channel.basic_publish(
+    exchange="data_exchange",
+    routing_key="",
+    body="AlgoAmais2",
+    properties=pika.BasicProperties(
+        delivery_mode=2
+    )
+)
+
